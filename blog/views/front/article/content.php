a91a78185a9057a1f678b896280dbcec31d4c2e8
ilei <wlm131127@163.com>
2015-05-31 14:07:00 +0800

    文案修改

--- blog/views/front/article/content.php
+++ blog/views/front/article/content.php
@@ -8,7 +8,7 @@
       <h1><?php echo htmlspecialchars($article['name']);?></h1>
       <p>
       <span class="left sj">时间:<?php echo date('Y-m-d', $article['updated_time']);?></span><span class="left fl">分类:<?php echo $article['cate_name'];?></span>
-      <span class="left author"><?php echo htmlspecialchars($article['author']);?></span>
+      <span class="left author"><?php echo 'ray'; //htmlspecialchars($article['author']);?></span>
       </p>
       <div class="clear"></div>
     </div>
