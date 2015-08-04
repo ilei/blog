5b3c5fe807df9f6debdbe3c47d9839e52a9c52a3
ilei <wlm131127@163.com>
2015-08-04 10:09:15 +0800

    修改seo关键字

--- blog/core/MY_Controller.php
+++ blog/core/MY_Controller.php
@@ -1,9 +1,9 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class MY_Controller extends CI_Controller{
 
-    protected $meta_keywords = array('IT', '互联网', 'web技术', '博客', 'ThinkLei');
-    protected $meta_desc     = 'ilei是一个专业的、有趣的、专注于将Web开发技术尽可能简明易懂地描述出来的IT博客，在这里你可以轻松愉快地学习Web开发技术。内容涵盖了php,javascript,linux,node.js,mysql,git,等多种web开发语言及技术';
-    protected $title         = array('ilei-享受编程-创造艺术');
+    protected $meta_keywords = array('PHP开发技术','PHP+MySQL开发', 'Node开发', 'web缓存技术', 'ThinkLei', 'GIT版本控制', '分布式部署');
+    protected $meta_desc     = 'ThinkLei 是一个专业的、有趣的、真实的将Web开发技术尽可能简明易懂地描述出来的互联网技术博客，在这里你可以轻松愉快地学习到PHP应用开发技术,PHP+MySQL开发与实践经验，Web的各种缓存及分布式的部署方案，NodeJs开发技术，GIT版本控制管理等耳熟能详的web开发技术';
+    protected $title         = array('ThinkLei-享受编程-创造艺术');
 
     protected $module        = array();
     protected $css           = array();
---
+++ push.php
@@ -0,0 +1,17 @@
+<?php 
+$urls = array(
+    'http://www.smartlei.com/article/24',
+    'http://www.smartlei.com/article/25',
+);
+$api = 'http://data.zz.baidu.com/urls?site=www.smartlei.com&token=yZqU5N8XbiKyZXI0';
+$ch = curl_init();
+$options =  array(
+    CURLOPT_URL => $api,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => implode("\n", $urls),
+    CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+);
+curl_setopt_array($ch, $options);
+$result = curl_exec($ch);
+echo $result;
